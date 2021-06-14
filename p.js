7fe324b83f9c1ebb79709d4c8a79de24089f73a4
milahnmartin <milahnmartin.develop@gmail.com>
2021-06-15 00:50:40 +0200

    initial database fix
    Added correct database extensions, removed dumb p javascript folder. created correct table with info and data

--- p.js
+++
@@ -1,6 +0,0 @@
-let nums = [1,2,3,4,5]
-let peeps = [6,7,8,9]
-let newnum = [...nums,...peeps]
-
-
-console.log(newnum)
--- server.js
+++ server.js
@@ -4,19 +4,20 @@ const PORT = 3001;
 
 
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('db.sqlite');
+const db = new sqlite3.Database('data.db');
 
+db.run("CREATE TABLE IF NOT EXISTS `users`(`id` INTEGER PRIMARY KEY AUTOINCREMENT,`name` TEXT,`info` BLOB default NULL);")
 
-app.get('/data/get/:name', (req,res,next)=>{
+app.get('/data/get/:name', (req, res, next) => {
     const query = req.params.name;
 
-   db.all(`SELECT * FROM players where name like '${query}%'`,(err,rows)=>{
-       if(rows){
-           res.json(rows)
-       }else{
-           res.json({status:"error"})
-       }
-   })
+    db.all(`SELECT * FROM players where name like '${query}%'`, (err, rows) => {
+        if (rows) {
+            res.json(rows)
+        } else {
+            res.json({ status: "error" })
+        }
+    })
 
 
 });
@@ -24,37 +25,37 @@ app.get('/data/get/:name', (req,res,next)=>{
 
 
 
-function new_player(pid,pname){
+function new_player(pid, pname) {
     db.run("insert into players(id,name)values($id,$name)",
-    {$id:pid,$name:pname},
-    (err,res) =>{
-        if(res)return true;
-        if(err)return false;
-    }
+        { $id: pid, $name: pname },
+        (err, res) => {
+            if (res) return true;
+            if (err) return false;
+        }
     )
 }
 
 
-app.get('/data/new/:name',(req,res)=>{
+app.get('/data/new/:name', (req, res) => {
     const _query_name = req.params.name;
     let highest_id = 0;
-    db.get("select id from players order by id desc limit 1",(err,rows)=>{
-        if(rows){
-         let new_id = rows.id + 1;
-            new_player(new_id,_query_name)
-            
-            if(new_player){
-                res.json({status:'SUCCESS'})
-            }else{
-                res.json({status:"ERROR"})
+    db.get("select id from players order by id desc limit 1", (err, rows) => {
+        if (rows) {
+            let new_id = rows.id + 1;
+            new_player(new_id, _query_name)
+
+            if (new_player) {
+                res.json({ status: 'SUCCESS' })
+            } else {
+                res.json({ status: "ERROR" })
             }
-            
+
         }
-    }) 
+    })
 
 })
 
-app.listen(PORT,(err,res)=>{
+app.listen(PORT, (err, res) => {
     console.log("SERVER RUNNING ON " + PORT)
 })
 
